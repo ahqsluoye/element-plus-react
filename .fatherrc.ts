a11b08001d8f8db39c0c2c8c079beac109f47ed7
Parker <ahqsluoye@126.com>
2025-03-30 18:38:53 +0800

    fix: 过时的api

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'father';
 export default defineConfig({
     // more father config: https://github.com/umijs/father/blob/master/docs/config.md
     // esm: { output: 'packages/esm' },
-    cjs: { output: 'packages/dist' },
+    esm: { output: 'packages/dist' },
 });
