9aea641ab444dd102ba84f631d86b828367dfe73
Parker <ahqsluoye@126.com>
2025-03-30 11:53:34 +0800

    添加字体路径变量

--- README.md
+++ README.md
@@ -13,15 +13,72 @@ import '@qsxy/element-plus-react/dist/index.css';
 import { ElButton, ElLink } from '@qsxy/element-plus-react';
 ```
 
-## Options
+在项目中改变 SCSS 变量
+element-plus-react 的 theme-chalk 使用 SCSS 编写，如果你的项目也使用了 SCSS，那么可以直接在项目中改变 Element 的样式变量。新建一个样式文件，例如 element-variables.scss，写入以下内容：
 
-TODO
+/_ 改变主题色变量 _/
+
+```scss
+@use 'sass:math';
+
+@mixin set-color-mix-level($number, $mode: 'light', $--el-color-primary, $mix-color: #ffffff) {
+    --el-color-primary-#{$mode}-#{$number}: #{mix($mix-color, $--el-color-primary, math.percentage(math.div($number, 10)))};
+}
+
+// Theme color variable
+// 主题色变量
+$--el-color-primary: #4f8ffa;
+:root {
+    // Theme color variable
+    // 主题色变量
+    --el-color-primary: #4f8ffa;
+    // Theme background color variable (button class)
+    // 主题背景颜色变量（按钮类）
+    --el-bg-color-primary: #4f8ffa;
+    // Theme border color variable (button class)
+    // 主题边框颜色变量（按钮类）
+    --btn-border-color: #48aff9;
+
+    @each $i in (3, 5, 7, 8, 9) {
+        @include set-color-mix-level($i, 'light', $--el-color-primary, #ffffff);
+    }
+
+    @include set-color-mix-level(2, 'dark', $--el-color-primary, #000000);
+
+    // dark mode 暗黑模式
+    &.is-dark {
+        --el-color-primary: #{mix(#141414, $--el-color-primary, 5)};
+
+        @each $i in (3, 5, 7, 8, 9) {
+            @include set-color-mix-level($i, 'light', $--el-color-primary, #141414);
+        }
+
+        @include set-color-mix-level(2, 'dark', $--el-color-primary, #141414);
+    }
+}
+```
+
+/_ 改变 icon 字体路径变量，必需 _/
+
+```scss
+$fa-font-path: '~@/assets/fonts';
+@import '@qsxy/element-plus-react/theme-chalk/index.scss';
+```
+
+## Warm Reminder 温馨提示
+
+Currently, this library is in the development and testing stage, and there may be some inevitable issues. If you encounter any problems, please feel free to raise an issue for me, and I will fix them as soon as possible.
+
+目前该库为开发测试阶段，会有一些不可避免的问题，如果您发现了一些问题，欢迎提 Issues 给我，我会尽快修复
 
 ## Development
 
 ```bash
 # install dependencies
 $ npm i @qsxy/element-plus-react
+# It is recommended to use (to avoid React version conflicts)
+# 推荐使用（避免react版本冲突）
+$ pnpm i @qsxy/element-plus-react
 ```
 
 ## LICENSE
--- gulpfile.js
+++ gulpfile.js
@@ -4,99 +4,20 @@ const del = require('del');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const through = require('through2');
-// const rename = require('gulp-rename');
-// 获取 minify-css 模块（用于压缩 CSS）
-// const babel = require('gulp-babel');
-// const babelrc = require('./babel.config');
-// const ts = require('gulp-typescript');
-// const tsProject = ts.createProject('tsconfig.json');
 const rename = require('gulp-rename');
+const fs = require('fs');
 
 const srcRoot = path.join(__dirname, './src');
-const libRoot = path.join(__dirname, './packages/lib');
-const styleRoot = path.join(__dirname, './packages/dist');
-const tsSources = [`${srcRoot}/components/**/*.tsx`, `${srcRoot}/components/**/*.ts`, `${srcRoot}/config/**/*.ts`, `!${srcRoot}/**/*.d.ts`];
+const styleRoot = path.join(__dirname, './packages/dist/');
 
 function clean(done) {
     del.sync([styleRoot], { force: true });
     done();
 }
-function cleanTemp(done) {
-    del.sync([path.join(__dirname, './lib')], { force: true });
-    done();
-}
-// function cleanES(done) {
-//     del.sync([esRoot], { force: true });
-//     done();
-// }
-
-// /**
-//  * 编译type文件
-//  * @returns
-//  */
-// function buildDts() {
-//     return gulp
-//         .src(tsSources, {
-//             ignore: ['**/demos/**/*', '**/tests/**/*', '*.patch.less'],
-//         })
-//         .pipe(tsProject())
-//         .dts.pipe(gulp.dest(libRoot));
-// }
-
-// function buildTypings() {
-//     return gulp.src(`${srcRoot}/typings/**.ts`).pipe(tsProject()).dts.pipe(gulp.dest('lib/es/typings')).pipe(gulp.dest('lib/cjs/typings'));
-// }
-
-// function buildIndex() {
-//     return gulp
-//         .src(`${srcRoot}/components/index.ts`)
-//         .pipe(babel(babelrc))
-//         .pipe(
-//             babel({
-//                 plugins: ['./babel-transform-sass-to-css'],
-//             }),
-//         )
-//         .pipe(gulp.dest(libRoot));
-// }
-
-// /**
-//  * 编译js文件
-//  * @returns
-//  */
-// function buildComponents() {
-//     return gulp
-//         .src(tsSources, {
-//             ignore: ['**/demos/**/*', '**/tests/**/*', '*.patch.less'],
-//         })
-//         .pipe(babel(babelrc))
-//         .pipe(
-//             babel({
-//                 plugins: ['./babel-transform-sass-to-css', './babel-plugin-module-resolver'],
-//             }),
-//         )
-//         .pipe(gulp.dest('lib/es'));
-//     // .pipe(gulp.dest(libRoot));
-// }
-
-// const buildES = gulp.parallel(/* buildIndex,  */ buildComponents);
-
-// function buildCJS() {
-//     return (
-//         gulp
-//             .src(['lib/es/**/*.js'])
-//             .pipe(
-//                 babel({
-//                     plugins: ['@babel/plugin-transform-modules-commonjs'],
-//                 }),
-//             )
-//             // .pipe(gulp.dest('lib/cjs/'));
-//             .pipe(gulp.dest(libRoot))
-//     );
-// }
 
 function buildComponentStyles() {
     return gulp
-        .src(`${srcRoot}/theme-chalk/index.scss`)
+        .src(`${srcRoot}/theme-chalk/build.scss`)
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename('index.css'))
         .pipe(gulp.dest(styleRoot));
@@ -113,6 +34,8 @@ function generatePackageJSON() {
                 delete parsed.peerDependencies;
                 delete parsed.devDependencies;
                 delete parsed.publishConfig;
+                delete parsed.commitlint;
+                delete parsed['lint-staged'];
                 const stringified = JSON.stringify(parsed, null, 2);
                 file.contents = Buffer.from(stringified);
                 cb(null, file);
@@ -134,21 +57,32 @@ function generateReadme() {
         .pipe(gulp.dest('./packages/'));
 }
 
-/**
- * 复制json文件
- * @returns
- */
-// function copyJson() {
-//     return gulp.src(srcRoot + '/components/config/icons.json').pipe(gulp.dest(libRoot + '/config'));
-// }
+function copyFiles(srcPath, destPath) {
+    if (!fs.existsSync(destPath)) {
+        fs.mkdirSync(destPath);
+    }
+    const files = fs.readdirSync(srcPath);
+    files.forEach(item => {
+        const stat = fs.statSync(srcPath + item);
+        if (stat.isDirectory()) {
+            //递归读取文件
+            copyFiles(srcPath + item + '/', destPath + item + '/');
+        } else {
+            fs.copyFile(srcPath + item, destPath + item, function (err) {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.log('something wrong was happened');
+                }
+            });
+        }
+    });
+}
 
-/**
- * 编译ts(x)文件
- */
-// const buildJs = gulp.parallel(buildDts, buildComponents, buildComponentStyles);
+function generateFonts() {
+    copyFiles(`${srcRoot}/theme-chalk/fonts/`, path.join(__dirname, './packages/dist/fonts/'));
+    copyFiles(`${srcRoot}/theme-chalk/`, path.join(__dirname, './packages/theme-chalk/'));
+    return gulp.src('./empty', { allowEmpty: true }).pipe(gulp.dest('./packages/dist/fonts/'));
+}
 
-// exports.build = gulp.series(clean, buildJs /* , buildCJS, cleanES */);
-
-// exports.build = gulp.series(clean, buildES, buildCJS, gulp.parallel(gulp.series(buildDts /* , buildTypings */), buildComponentStyles, generatePackageJSON), cleanTemp);
-exports.build = gulp.parallel(buildComponentStyles, generatePackageJSON, generateReadme);
+exports.build = gulp.series(gulp.parallel(buildComponentStyles, generatePackageJSON, generateReadme, generateReadme), generateFonts);
 exports.clean = clean;
