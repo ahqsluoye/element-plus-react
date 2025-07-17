333123fd1a5d6972ef76a6fc80e461617990d707
Parker <ahqsluoye@126.com>
2025-07-17 22:54:06 +0800

    文档首页

--- .dumi/theme/layouts/DocLayout/index.tsx
+++ .dumi/theme/layouts/DocLayout/index.tsx
@@ -4,7 +4,7 @@ import Main from '@/theme/slots/Main';
 import Sidebar from '@/theme/slots/Sidebar';
 import { ElConfigProvider } from '@qsxy/element-plus-react';
 import '@qsxy/element-plus-react/theme-chalk/dark/css-vars.scss';
-import '@qsxy/element-plus-react/theme-chalk/doc.scss';
+import '@qsxy/element-plus-react/theme-chalk/dev.scss';
 import { Helmet, useIntl, useRouteMeta, useSidebarData } from 'dumi';
 import React, { FC, memo } from 'react';
 import './style';
