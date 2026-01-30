1196efe49aa6c0183b0a8531a1e74bf6795d9fdd
Parker <ahqsluoye@126.com>
2026-01-30 19:21:18 +0800

    text组件

--- docs/components/text/mixed.tsx
+++ docs/components/text/mixed.tsx
@@ -1,8 +1,29 @@
-import { ElText } from '@qsxy/element-plus-react';
+import { ElButton, ElIcon, ElText } from '@qsxy/element-plus-react';
 import React from 'react';
 
 const App = () => {
-    return <ElText></ElText>;
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', columnGap: 15, width: 360 }}>
+            <ElText>
+                <ElIcon name="home" />
+                Element-Plus
+            </ElText>
+            <div>
+                <ElText>Rate</ElText>
+                <ElIcon name="star" />
+                <ElIcon name="star" />
+                <ElIcon name="star" />
+                <ElIcon name="star" />
+                <ElIcon name="star" />
+            </div>
+            <ElText>
+                This is text mixed icon
+                <ElIcon name="bell" />
+                and component
+                <ElButton>Button</ElButton>
+            </ElText>
+        </div>
+    );
 };
 
 export default App;
