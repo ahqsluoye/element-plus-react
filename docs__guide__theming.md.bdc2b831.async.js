97cec902e186241939a0c1e6fdd64965e24d7fe8
Parker <ahqsluoye@126.com>
2026-06-04 17:25:55 +0800

    Updates

---
+++ docs__guide__theming.md.bdc2b831.async.js
@@ -0,0 +1,72 @@
+"use strict";(self.webpackChunk_qsxy_element_plus_react=self.webpackChunk_qsxy_element_plus_react||[]).push([[2804],{35370:function(_,n,s){s.r(n);var r=s(65551),x=s(10074),p=s(3826),l=s(95249),m=s(35317),t=s(4329),v=s(27909),h=s(98157),d=s(99058),I=s(34112),j=s(16394),E=s(62569),D=s(65422),f=s(52154),c=s(72276),u=s(99170),i=s(75271),a=s(96485),e=s(52676);function o(){return(0,e.jsx)(c.dY,{children:(0,e.jsx)(i.Suspense,{fallback:(0,e.jsx)(u.Z,{}),children:(0,e.jsxs)(e.Fragment,{children:[(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"\u81EA\u5B9A\u4E49\u4E3B\u9898",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u81EA\u5B9A\u4E49\u4E3B\u9898",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u81EA\u5B9A\u4E49\u4E3B\u9898"]}),(0,e.jsx)("p",{children:a.texts[0].value}),(0,e.jsx)("p",{children:a.texts[1].value}),(0,e.jsxs)("h2",{id:"\u66F4\u6362\u4E3B\u9898\u8272",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u66F4\u6362\u4E3B\u9898\u8272",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u66F4\u6362\u4E3B\u9898\u8272"]}),(0,e.jsxs)("h3",{id:"\u901A\u8FC7-scss-\u53D8\u91CF",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u901A\u8FC7-scss-\u53D8\u91CF",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u901A\u8FC7 SCSS \u53D8\u91CF"]}),(0,e.jsxs)("p",{children:[(0,e.jsx)("code",{children:a.texts[2].value}),a.texts[3].value,(0,e.jsx)(d.Z,{href:"https://github.com/ahqsluoye/element-plus-react/blob/main/src/theme-chalk/common/var.scss",children:a.texts[4].value}),a.texts[5].value]}),(0,e.jsx)(t.Z,{lang:"scss",children:a.texts[6].value}),(0,e.jsxs)("h3",{id:"\u5982\u4F55\u8986\u76D6\u5B83",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u5982\u4F55\u8986\u76D6\u5B83",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u5982\u4F55\u8986\u76D6\u5B83\uFF1F"]}),(0,e.jsxs)("p",{children:[a.texts[7].value,(0,e.jsx)("code",{children:a.texts[8].value}),a.texts[9].value]})]}),(0,e.jsxs)(l.Z,{type:"error",title:"WARNING",children:[(0,e.jsxs)("p",{children:[a.texts[10].value,(0,e.jsx)("code",{children:a.texts[11].value}),a.texts[12].value,(0,e.jsx)("code",{children:a.texts[13].value}),a.texts[14].value]}),(0,e.jsxs)("p",{children:[a.texts[15].value,(0,e.jsx)("code",{children:a.texts[16].value}),a.texts[17].value]}),(0,e.jsx)("blockquote",{children:(0,e.jsxs)("p",{children:[(0,e.jsx)("a",{href:"https://sass-lang.com/documentation/at-rules/use",children:a.texts[18].value}),a.texts[19].value,(0,e.jsx)("a",{href:"https://sass-lang.com/documentation/at-rules/import",children:a.texts[20].value})]})})]}),(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)(t.Z,{title:"styles/element/index.scss",lang:"scss",children:a.texts[21].value}),(0,e.jsx)("p",{children:a.texts[22].value})]}),(0,e.jsx)(l.Z,{type:"info",title:"TIP",children:(0,e.jsxs)("p",{children:[a.texts[23].value,(0,e.jsx)("code",{children:a.texts[24].value}),a.texts[25].value]})}),(0,e.jsx)("div",{className:"markdown",children:(0,e.jsxs)("p",{children:[a.texts[26].value,(0,e.jsx)("code",{children:a.texts[27].value}),a.texts[28].value]})}),(0,e.jsx)(l.Z,{type:"info",title:"TIP",children:(0,e.jsxs)("p",{children:[a.texts[29].value,(0,e.jsx)("code",{children:a.texts[30].value}),a.texts[31].value]})}),(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)(t.Z,{title:"main.ts",lang:"ts",children:a.texts[32].value}),(0,e.jsxs)("h3",{id:"\u901A\u8FC7-css-\u53D8\u91CF\u8BBE\u7F6E",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u901A\u8FC7-css-\u53D8\u91CF\u8BBE\u7F6E",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u901A\u8FC7 CSS \u53D8\u91CF\u8BBE\u7F6E"]}),(0,e.jsx)("p",{children:a.texts[33].value}),(0,e.jsx)("blockquote",{children:(0,e.jsxs)("p",{children:[a.texts[34].value,(0,e.jsx)("a",{href:"https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties",children:a.texts[35].value}),a.texts[36].value]})}),(0,e.jsx)("p",{children:a.texts[37].value}),(0,e.jsx)("div",{children:(0,e.jsx)("p",{children:a.texts[38].value})}),(0,e.jsx)("p",{children:a.texts[39].value}),(0,e.jsx)("blockquote",{children:(0,e.jsx)("p",{children:a.texts[40].value})}),(0,e.jsx)("p",{children:a.texts[41].value}),(0,e.jsx)(t.Z,{lang:"css",children:a.texts[42].value}),(0,e.jsx)("p",{children:a.texts[43].value}),(0,e.jsx)(t.Z,{lang:"ts",children:a.texts[44].value}),(0,e.jsxs)("p",{children:[a.texts[45].value,(0,e.jsx)("code",{children:a.texts[46].value}),a.texts[47].value]}),(0,e.jsx)(t.Z,{lang:"css",children:a.texts[48].value}),(0,e.jsx)("p",{children:a.texts[49].value}),(0,e.jsx)(t.Z,{lang:"ts",children:a.texts[50].value}),(0,e.jsxs)("p",{children:[a.texts[51].value,(0,e.jsx)("a",{href:"https://vueuse.org/core/usecssvar/",children:a.texts[52].value})]})]})]})})})}n.default=o},96485:function(_,n,s){s.r(n),s.d(n,{texts:function(){return r}});const r=[{value:"Element Plus \u9ED8\u8BA4\u63D0\u4F9B\u4E00\u5957\u4E3B\u9898\uFF0CCSS \u547D\u540D\u91C7\u7528 BEM \u7684\u98CE\u683C\uFF0C\u65B9\u4FBF\u4F7F\u7528\u8005\u8986\u76D6\u6837\u5F0F\u3002 \u4F46\u662F\u5982\u679C\u9700\u8981\u5927\u89C4\u6A21\u66FF\u6362\u6837\u5F0F\uFF0C\u4F8B\u5982\uFF1A \u5C06\u4E3B\u9898\u989C\u8272\u4ECE\u84DD\u8272\u6539\u4E3A\u6A59\u8272\u6216\u7EFF\u8272\uFF0C\u4E5F\u8BB8\u4E00\u4E2A\u4E2A\u5C06\u5176\u8986\u76D6\u8D77\u6765\u4E0D\u662F\u4E00\u4E2A\u597D\u4E3B\u610F\u3002",paraId:0,tocIndex:0},{value:"\u6211\u4EEC\u63D0\u4F9B\u56DB\u79CD\u65B9\u6CD5\u6765\u6539\u53D8\u6837\u5F0F\u53D8\u91CF\u3002",paraId:1,tocIndex:0},{value:"theme-chalk",paraId:2,tocIndex:2},{value:" \u4F7F\u7528 SCSS \u7F16\u5199\u800C\u6210\u3002 \u4F60\u53EF\u4EE5\u5728 ",paraId:2,tocIndex:2},{value:"src/theme-chalk/common/var.scss",paraId:3,tocIndex:2},{value:" \u6587\u4EF6\u4E2D\u67E5\u627E SCSS \u53D8\u91CF\u3002",paraId:2,tocIndex:2},{value:`$colors: () !default;\r
+$colors: map.deep-merge(\r
+    (\r
+        'white': #ffffff,\r
+        'black': #000000,\r
+        'primary': (\r
+            'base': #409eff,\r
+        ),\r
+        'success': (\r
+            'base': #67c23a,\r
+        ),\r
+        'warning': (\r
+            'base': #e6a23c,\r
+        ),\r
+        'danger': (\r
+            'base': #f56c6c,\r
+        ),\r
+        'error': (\r
+            'base': #f56c6c,\r
+        ),\r
+        'info': (\r
+            'base': #909399,\r
+        ),\r
+    ),\r
+    $colors\r
+);
+`,paraId:4,tocIndex:2},{value:"\u5982\u679C\u60A8\u7684\u9879\u76EE\u4E5F\u4F7F\u7528\u4E86 SCSS\uFF0C\u53EF\u4EE5\u76F4\u63A5\u4FEE\u6539 Element Plus \u7684\u6837\u5F0F\u53D8\u91CF\u3002 \u65B0\u5EFA\u4E00\u4E2A\u6837\u5F0F\u6587\u4EF6\uFF0C\u4F8B\u5982 ",paraId:5,tocIndex:3},{value:"styles/element/index.scss",paraId:5,tocIndex:3},{value:"\uFF1A",paraId:5,tocIndex:3},{value:"\u60A8\u5E94\u8BE5\u4F7F\u7528 ",paraId:6},{value:"@use 'xxx.scss' as *;",paraId:6},{value:" \u4EE3\u66FF ",paraId:6},{value:"@import 'xxx.scss';",paraId:6},{value:"\u3002",paraId:6},{value:"\u56E0\u4E3A sass \u56E2\u961F\u8BF4\u4ED6\u4EEC\u6700\u7EC8\u4F1A\u5220\u9664 ",paraId:7},{value:"@import",paraId:7},{value:" \u8BED\u6CD5\u3002",paraId:7},{value:"Sass: @use",paraId:8},{value:" vs ",paraId:8},{value:"Sass: @import",paraId:8},{value:`/* just override what you need */\r
+@use '@qsxy/element-plus-react/theme-chalk/common/var' with(\r
+        // \u5B57\u4F53\u6587\u4EF6\u8DEF\u5F84\u5FC5\u586B\r
+        $fa-font-path: '~/node_modules/@qsxy/element-plus-react/theme-chalk/fonts',\r
+        // \u5176\u4ED6\u53D8\u91CF\u53EF\u81EA\u5B9A\u4E49\r
+        $colors:\r
+            (\r
+                'primary': (\r
+                    'base': green,\r
+                )\r
+            )\r
+    );\r
+\r
+// If you just import on demand, you can ignore the following content.\r
+// \u5982\u679C\u4F60\u60F3\u5BFC\u5165\u6240\u6709\u6837\u5F0F:\r
+// @use '@qsxy/element-plus-react/theme-chalk/common/var'\r
+//     with(\r
+//         $fa-font-path: '~/node_modules/@qsxy/element-plus-react/theme-chalk/fonts',\r
+//     );\r
+// @use "@qsxy/element-plus-react/theme-chalk/index.scss" as *;
+`,paraId:9},{value:"\u7136\u540E\u5728\u4F60\u7684\u9879\u76EE\u5165\u53E3\u6587\u4EF6\u4E2D\uFF0C\u5BFC\u5165\u8FD9\u4E2A\u6837\u5F0F\u6587\u4EF6\u4EE5\u66FF\u6362 Element Plus React \u5185\u7F6E\u7684 CSS\uFF1A",paraId:10},{value:"\u5728 element-plus-react scss \u6587\u4EF6\u4E4B\u524D\u5BFC\u5165",paraId:11},{value:"element/index.scss",paraId:11},{value:"\u4EE5\u907F\u514D sass \u6DF7\u5408\u53D8\u91CF\u7684\u95EE\u9898\uFF0C\u56E0\u4E3A\u6211\u4EEC\u9700\u8981\u901A\u8FC7\u4F60\u7684\u81EA\u5B9A\u4E49\u53D8\u91CF\u751F\u6210 light-x\u3002",paraId:11},{value:"\u521B\u5EFA\u4E00\u4E2A ",paraId:12},{value:"element/index.scss",paraId:12},{value:" \u6587\u4EF6\u6765\u5408\u5E76\u4F60\u7684\u53D8\u91CF\u548C element-plus-react \u7684\u53D8\u91CF\u3002 \uFF08\u5982\u679C\u4F60\u5728 TypeScript \u4E2D\u5BFC\u5165\u4E86\u5B83\u4EEC\uFF0C\u4ED6\u4EEC\u5C06\u4E0D\u4F1A\u88AB\u5408\u5E76\uFF09",paraId:12},{value:"\u9664\u6B64\u4EE5\u5916\uFF0C\u4F60\u5E94\u8BE5\u5C06\u4F60\u7684 scss \u6587\u4EF6\u4E0E element \u53D8\u91CF\u7684 scss \u6587\u4EF6\u533A\u5206\u5F00\u6765\u3002 \u5982\u679C\u5C06\u5B83\u4EEC\u6DF7\u5408\u5728\u4E00\u8D77\uFF0C",paraId:13},{value:"element-plus-react",paraId:13},{value:" \u6BCF\u6B21\u70ED\u66F4\u65B0\u90FD\u9700\u8981\u7F16\u8BD1\u5927\u91CF\u7684 scss \u6587\u4EF6\uFF0C\u8FD9\u5C06\u4F1A\u5BFC\u81F4\u7F16\u8BD1\u901F\u5EA6\u53D8\u6162\u3002",paraId:13},{value:`import React from 'react';\r
+import { createRoot } from 'react-dom/client';\r
+import './styles/element/index.scss';\r
+import '@qsxy/element-plus-react/theme-chalk/index.scss';\r
+import App from './App';\r
+\r
+const renderDom = document.getElementById('root');\r
+const root = createRoot(renderDom);\r
+root.render(<App />);
+`,paraId:14},{value:"CSS \u53D8\u91CF\u662F\u4E00\u4E2A\u975E\u5E38\u6709\u7528\u7684\u529F\u80FD\uFF0C\u51E0\u4E4E\u6240\u6709\u6D4F\u89C8\u5668\u90FD\u652F\u6301\u3002 \uFF08IE\uFF1A\u554A\u8FD9\uFF1F)",paraId:15,tocIndex:4},{value:"\u4ECE ",paraId:16,tocIndex:4},{value:"\u4F7F\u7528 CSS \u81EA\u5B9A\u4E49\u5C5E\u6027(\u53D8\u91CF) | MDN",paraId:16,tocIndex:4},{value:" \u4E86\u89E3\u66F4\u591A\u4FE1\u606F",paraId:16,tocIndex:4},{value:"\u6211\u4EEC\u7528 css \u53D8\u91CF\u6765\u91CD\u6784\u4E86\u51E0\u4E4E\u6240\u6709\u7EC4\u4EF6\u7684\u6837\u5F0F\u7CFB\u7EDF\u3002",paraId:17,tocIndex:4},{value:"\u5B83\u517C\u5BB9 SCSS \u53D8\u91CF\u7CFB\u7EDF\u3002 \u6211\u4EEC\u4F7F\u7528 SCSS \u7684\u51FD\u6570\u81EA\u52A8\u751F\u6210\u9700\u8981\u7528\u5230\u7684 css \u53D8\u91CF\u3002",paraId:18,tocIndex:4},{value:"\u8FD9\u610F\u5473\u7740\u4F60\u53EF\u4EE5\u52A8\u6001\u5730\u6539\u53D8\u7EC4\u4EF6\u5185\u7684\u4E2A\u522B\u53D8\u91CF\uFF0C\u4EE5\u4FBF\u66F4\u597D\u5730\u81EA\u5B9A\u4E49\u7EC4\u4EF6\u6837\u5F0F\uFF0C\u800C\u4E0D\u9700\u8981\u4FEE\u6539 SCSS \u6587\u4EF6\u91CD\u65B0\u7F16\u8BD1\u4E00\u6B21\u3002",paraId:19,tocIndex:4},{value:"\u4E4B\u540E\uFF0C\u6BCF\u4E2A\u7EC4\u4EF6\u7684 css \u53D8\u91CF\u540D\u79F0\u548C\u4F5C\u7528\u5C06\u88AB\u5199\u5165\u5230\u5BF9\u5E94\u7684\u7EC4\u4EF6\u6587\u6863\u4E2D\u3002",paraId:20,tocIndex:4},{value:"\u5C31\u50CF\u8FD9\u6837\uFF1A",paraId:21,tocIndex:4},{value:`:root {\r
+    --el-color-primary: green;\r
+}
+`,paraId:22,tocIndex:4},{value:"\u5982\u679C\u4F60\u53EA\u60F3\u81EA\u5B9A\u4E49\u4E00\u4E2A\u7279\u5B9A\u7684\u7EC4\u4EF6\uFF0C\u53EA\u9700\u4E3A\u67D0\u4E9B\u7EC4\u4EF6\u5355\u72EC\u6DFB\u52A0\u5185\u8054\u6837\u5F0F\u3002",paraId:23,tocIndex:4},{value:`<ElTag style={{ '--el-tag-bg-color': 'red' }}>Tag</ElTag>
+`,paraId:24,tocIndex:4},{value:"\u51FA\u4E8E\u6027\u80FD\u539F\u56E0\uFF0C\u66F4\u52A0\u63A8\u8350\u4F60\u5728\u7C7B\u540D\u4E0B\u6DFB\u52A0\u81EA\u5B9A\u4E49 css \u53D8\u91CF\uFF0C\u800C\u4E0D\u662F\u5728\u5168\u5C40\u7684 ",paraId:25,tocIndex:4},{value:":root",paraId:25,tocIndex:4},{value:" \u4E0B\u3002",paraId:25,tocIndex:4},{value:`.custom-class {\r
+    --el-tag-bg-color: red;\r
+}
+`,paraId:26,tocIndex:4},{value:"\u5982\u679C\u60A8\u60F3\u8981\u901A\u8FC7 js \u63A7\u5236 css \u53D8\u91CF\uFF0C\u53EF\u4EE5\u8FD9\u6837\u505A\uFF1A",paraId:27,tocIndex:4},{value:`// document.documentElement \u662F\u5168\u5C40\u53D8\u91CF\u65F6\r
+const el = document.documentElement;\r
+// const el = document.getElementById('xxx')\r
+\r
+// \u83B7\u53D6 css \u53D8\u91CF\r
+getComputedStyle(el).getPropertyValue(\`--el-color-primary\`);\r
+\r
+// \u8BBE\u7F6E css \u53D8\u91CF\r
+el.style.setProperty('--el-color-primary', 'red');
+`,paraId:28,tocIndex:4},{value:"\u5982\u679C\u4F60\u60F3\u8981\u66F4\u4F18\u96C5\u7684\u65B9\u5F0F\uFF0C\u8BF7\u770B\u8FD9\u91CC\u3002 ",paraId:29,tocIndex:4},{value:"useCssVar | VueUse",paraId:29,tocIndex:4}]}}]);
