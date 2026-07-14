fe06f7965781239f9c112e7b160cc12e74bf186a
Parker <ahqsluoye@126.com>
2026-07-14 14:27:19 +0800

    chore: release v2.0.1 and update demo code
    1. bump version to 2.0.1
    2. fix test script path to scripts/copyLib.js
    3. update dialog demo to add more form components and fields

--- docs/components/dialog/customization-content.tsx
+++ docs/components/dialog/customization-content.tsx
@@ -1,9 +1,29 @@
-import { ElButton, ElDialog, ElForm, ElFormItem, ElInput, ElMessageBox, useForm } from '@qsxy/element-plus-react';
+import {
+    ElButton,
+    ElCheckbox,
+    ElCheckboxGroup,
+    ElCol,
+    ElDatePicker,
+    ElDialog,
+    ElForm,
+    ElFormItem,
+    ElInput,
+    ElMessageBox,
+    ElOption,
+    ElRadio,
+    ElRadioGroup,
+    ElRow,
+    ElSelect,
+    ElSwitch,
+    ElTextArea,
+    ElTimePicker,
+    useForm,
+} from '@qsxy/element-plus-react';
 import React, { useCallback, useState } from 'react';
 
 const App = () => {
     const [formInstance] = useForm();
-    const initialValues = { userName: '张三' };
+    const initialValues = { userName: '张三', address: { province: '2' } };
 
     const [visible, setVisible] = useState(false);
     const onClose = useCallback((done: (cancle: boolean) => void) => {
@@ -25,18 +45,56 @@ const App = () => {
         <>
             <ElButton onClick={() => setVisible(true)}>打开对话框</ElButton>
             <ElDialog visible={visible} beforeClose={onClose} onCloseDialog={() => setVisible(false)}>
-                <ElForm form={formInstance} initialValues={initialValues}>
-                    <ElFormItem
-                        label="名称"
-                        name="userName"
-                        rules={[
-                            {
-                                required: true,
-                                message: '名称不能为空哦',
-                            },
-                        ]}
-                    >
+                <ElForm form={formInstance} initialValues={initialValues} labelWidth={80}>
+                    <ElFormItem name="name" label="活动名称">
                         <ElInput />
+                    </ElFormItem>
+                    <ElFormItem name="region" label="活动区域">
+                        <ElSelect placeholder="请选择活动区域">
+                            <ElOption label="区域一" value="shanghai" />
+                            <ElOption label="区域二" value="beijing" />
+                        </ElSelect>
+                    </ElFormItem>
+                    <ElFormItem label="活动时间">
+                        <ElRow style={{ width: '100%' }}>
+                            <ElCol span={11}>
+                                <ElFormItem name={['date', 'date1']} noStyle>
+                                    <ElDatePicker type="date" placeholder="选择日期" />
+                                </ElFormItem>
+                            </ElCol>
+                            <ElCol span={2} style={{ textAlign: 'center' }}>
+                                <span style={{ color: 'var(--el-text-color-placeholder)' }}>-</span>
+                            </ElCol>
+                            <ElCol span={11}>
+                                <ElFormItem name={['date', 'date2']} noStyle>
+                                    <ElTimePicker placeholder="选择时间" />
+                                </ElFormItem>
+                            </ElCol>
+                        </ElRow>
+                    </ElFormItem>
+                    <ElFormItem name="delivery" label="即时配送">
+                        <ElSwitch />
+                    </ElFormItem>
+                    <ElFormItem name="type" label="活动性质">
+                        <ElCheckboxGroup name="type">
+                            <ElCheckbox value="Online activities">美食/餐厅线上活动</ElCheckbox>
+                            <ElCheckbox value="Promotion activities">地推活动</ElCheckbox>
+                            <ElCheckbox value="Offline activities">线下主题活动</ElCheckbox>
+                            <ElCheckbox value="Simple brand exposure">单纯品牌曝光</ElCheckbox>
+                        </ElCheckboxGroup>
+                    </ElFormItem>
+                    <ElFormItem name="resource" label="特殊资源">
+                        <ElRadioGroup>
+                            <ElRadio value="Sponsor">线上品牌商赞助</ElRadio>
+                            <ElRadio value="Venue">线下场地免费</ElRadio>
+                        </ElRadioGroup>
+                    </ElFormItem>
+                    <ElFormItem name="desc" label="活动形式">
+                        <ElTextArea />
+                    </ElFormItem>
+                    <ElFormItem>
+                        <ElButton type="primary">立即创建</ElButton>
+                        <ElButton>取消</ElButton>
                     </ElFormItem>
                 </ElForm>
             </ElDialog>
