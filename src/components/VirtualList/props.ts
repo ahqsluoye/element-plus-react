989dccef8fa333100dc354589c715dd00fdd3d91
Parker <ahqsluoye@126.com>
2026-07-16 14:49:45 +0800

    refactor(VirtualList): 修正TableV2组件导入路径
    将TableGridRowSlotParams的导入路径从../TableV2/src/table改为../TableV2/table，保持导入路径一致性

--- src/components/VirtualList/props.ts
+++ src/components/VirtualList/props.ts
@@ -1,7 +1,7 @@
 import { VERTICAL } from './defaults';
 
 import React, { CSSProperties } from 'react';
-import { TableGridRowSlotParams } from '../TableV2/src/table';
+import { TableGridRowSlotParams } from '../TableV2/table';
 import type { Dir, GridItemKeyGetter, ItemSize } from './types';
 
 export interface VirtualizedProps {
