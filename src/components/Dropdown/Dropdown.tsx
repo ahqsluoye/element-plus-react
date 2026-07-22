451f966ea033599bbb2c1e772c2d86b2bf71d385
Parker <ahqsluoye@126.com>
2026-07-22 15:34:43 +0800

    feat(Dropdown): 新增拆分按钮、虚拟触发等多项功能
    1.  扩展DropdownProps类型，支持string/number作为子节点，新增splitButton、type、size、buttonProps等十余项属性
    2.  实现拆分按钮模式，支持按钮组形式的触发UI
    3.  新增虚拟触发相关支持，适配popper虚拟触发场景
    4.  重构事件回调命名，修正onVisiblechange为onVisibleChange，拆分点击事件为onClick和onCommand
    5.  添加滚动容器支持，当设置maxHeight时使用Scrollbar包裹菜单
    6.  新增主题effect参数支持，默认主题改为light
    7.  优化类型导入和代码结构，使用内置hooks统一处理disabled和size逻辑

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,13 +1,18 @@
+import ElButton from '@qsxy/element-plus-react/Button/Button';
+import ElButtonGroup from '@qsxy/element-plus-react/Button/ButtonGroup';
+import ElScrollbar from '@qsxy/element-plus-react/Scrollbar/Scrollbar';
 import ElTooltip from '@qsxy/element-plus-react/Tooltip/Tooltip';
+import type { TooltipRef } from '@qsxy/element-plus-react/Tooltip/typings';
 import { mergeDefaultProps } from '@qsxy/element-plus-react/Util/base';
 import { partitionAnimationProps } from '@qsxy/element-plus-react/hooks/animationPropsUtils';
 import { partitionHTMLProps } from '@qsxy/element-plus-react/hooks/htmlPropsUtils';
 import { partitionPopperPropsUtils } from '@qsxy/element-plus-react/hooks/popperPropsUtils';
 import useClassNames from '@qsxy/element-plus-react/hooks/useClassNames';
+import { useDisabled, useSize } from '@qsxy/element-plus-react/hooks/useCommonProps';
 import classNames from 'classnames';
-import React, { forwardRef, useCallback, useImperativeHandle, useRef } from 'react';
+import React, { forwardRef, isValidElement, useCallback, useImperativeHandle, useMemo, useRef } from 'react';
 import { DropdownContext } from './DropdownContext';
-import { DropdownProps, DropdownRef } from './typings';
+import type { DropdownProps, DropdownRef } from './typings';
 
 const Dropdown = forwardRef<DropdownRef, DropdownProps>((props, ref) => {
     props = mergeDefaultProps(
@@ -17,6 +22,7 @@ const Dropdown = forwardRef<DropdownRef, DropdownProps>((props, ref) => {
             showTimeout: 0,
             hideTimeout: 100,
             trigger: 'hover',
+            effect: 'light',
         },
         props,
     );
@@ -29,66 +35,122 @@ const Dropdown = forwardRef<DropdownRef, DropdownProps>((props, ref) => {
         trigger,
         hideOnClick,
         onClick,
-        onVisiblechange,
+        onVisibleChange,
         onEnter,
         onMouseEnter,
         onMouseLeave,
-        disabled,
+        virtualTriggering,
+        virtualRef,
+        effect,
+        maxHeight,
+        splitButton,
+        type,
+        buttonProps,
+        onCommand,
         ...rest
     } = props;
     const [popperProps] = partitionPopperPropsUtils(rest);
     const [transitionProps] = partitionAnimationProps(rest);
-    const [tooltipEvents] = partitionHTMLProps(props, { htmlProps: ['onMouseEnter', 'onMouseLeave', 'onClick', 'onContextMenu'] });
+    const [tooltipEvents] = partitionHTMLProps(props, { htmlProps: ['onMouseEnter', 'onMouseLeave', 'onContextMenu'] });
 
     const { b, e, is } = useClassNames(classPrefix);
     const containerRef = useRef(null);
+    const tooltipRef = useRef<TooltipRef>(null);
+
+    const disabled = useDisabled(props.disabled);
+    const size = useSize(props.size);
 
     /** 显示 */
     const handleMouseEnter = useCallback(
-        (event: React.MouseEvent<any>) => {
+        (event?: React.MouseEvent<any>) => {
             onMouseEnter?.(event);
-            onVisiblechange?.(true);
+            onVisibleChange?.(true);
         },
-        [onMouseEnter, onVisiblechange],
+        [onMouseEnter, onVisibleChange],
     );
 
     /** 隐藏 */
     const handleMouseLeave = useCallback(
-        (event: React.MouseEvent<any>) => {
+        (event?: React.MouseEvent<any>) => {
             onMouseLeave?.(event);
-            onVisiblechange?.(false);
+            onVisibleChange?.(false);
         },
-        [onMouseLeave, onVisiblechange],
+        [onMouseLeave, onVisibleChange],
     );
 
     useImperativeHandle(ref, () => ({
         ref: containerRef,
-        handleOpen: onMouseEnter,
-        handleClose: onMouseLeave,
+        handleOpen: () => tooltipRef.current?.onOpen(),
+        handleClose: () => tooltipRef.current?.onClose(),
     }));
+
+    const triggerRef = useMemo(() => {
+        if (isValidElement(props.children)) {
+            return props.children;
+        }
+        return <span>{props.children}</span>;
+    }, [props.children]);
 
     return (
         <div ref={containerRef} className={classNames(b(), props.className)} style={props.style} {...tooltipEvents}>
-            <ElTooltip
-                classPrefix={classPrefix}
-                triggerRef={props.children}
-                popperClass={classNames(e`popper`, is`pure`)}
-                disabled={disabled}
-                enterable
-                hideOnClick={hideOnClick}
-                effect="light"
-                offset={offset}
-                showAfter={showTimeout}
-                hideAfter={hideTimeout}
-                trigger={trigger}
-                onEnter={onEnter}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                contentSlot={<DropdownContext.Provider value={{ hideOnClick, onClick }}>{menu}</DropdownContext.Provider>}
-                unmountOnExit
-                {...popperProps}
-                {...transitionProps}
-            />
+            {splitButton ? (
+                <ElButtonGroup size={size}>
+                    <ElButton {...buttonProps} type={type} size={size} disabled={disabled} onClick={onClick}>
+                        {triggerRef}
+                    </ElButton>
+                    <ElTooltip
+                        ref={tooltipRef}
+                        classPrefix={classPrefix}
+                        triggerRef={<ElButton {...buttonProps} className={e`caret-button`} type={type} size={size} disabled={disabled} icon="angle-down" />}
+                        virtualRef={virtualRef}
+                        virtualTriggering={virtualTriggering}
+                        popperClass={classNames(e`popper`, is`pure`)}
+                        disabled={disabled}
+                        enterable
+                        hideOnClick={hideOnClick}
+                        effect={effect}
+                        offset={offset}
+                        showAfter={showTimeout}
+                        hideAfter={hideTimeout}
+                        trigger={trigger}
+                        onEnter={onEnter}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        contentSlot={<DropdownContext.Provider value={{ hideOnClick, onCommand: onClick, maxHeight, size }}>{menu}</DropdownContext.Provider>}
+                        unmountOnExit
+                        {...popperProps}
+                        {...transitionProps}
+                    />
+                </ElButtonGroup>
+            ) : (
+                <ElTooltip
+                    ref={tooltipRef}
+                    classPrefix={classPrefix}
+                    triggerRef={triggerRef}
+                    virtualRef={virtualRef}
+                    virtualTriggering={virtualTriggering}
+                    popperClass={classNames(e`popper`, is`pure`)}
+                    disabled={disabled}
+                    enterable
+                    hideOnClick={hideOnClick}
+                    effect={effect}
+                    offset={offset}
+                    showAfter={showTimeout}
+                    hideAfter={hideTimeout}
+                    trigger={trigger}
+                    onEnter={onEnter}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                    contentSlot={
+                        <DropdownContext.Provider value={{ hideOnClick, onCommand }}>
+                            {maxHeight ? <ElScrollbar maxHeight={maxHeight}>{menu}</ElScrollbar> : menu}
+                        </DropdownContext.Provider>
+                    }
+                    unmountOnExit
+                    {...popperProps}
+                    {...transitionProps}
+                />
+            )}
         </div>
     );
 });
--- src/components/Dropdown/typings.ts
+++ src/components/Dropdown/typings.ts
@@ -1,3 +1,5 @@
+import { VirtualElement } from '@popperjs/core';
+import { ButtonProps } from '@qsxy/element-plus-react//Button/typings';
 import { PopperOptions } from '@qsxy/element-plus-react/Popper/typings';
 import { TransitionProps } from '@qsxy/element-plus-react/Transition/Transition';
 import { AnimationEventProps, BaseProps, NativeProps } from '@qsxy/element-plus-react/types/common';
@@ -5,7 +7,8 @@ import React, { RefObject } from 'react';
 
 /** Dropdown 属性 */
 export interface DropdownProps
-    extends BaseProps<React.ReactElement>,
+    extends
+        BaseProps<React.ReactElement | string | number>,
         NativeProps<'--el-dropdown-menu-box-shadow-light' | '--el-dropdown-menuItem-hover-fill' | '--el-dropdown-menuItem-hover-color'>,
         PopperOptions,
         AnimationEventProps,
@@ -13,6 +16,18 @@ export interface DropdownProps
     menu: React.ReactElement<DropdownMenuProps>;
     /** 触发下拉的行为 */
     trigger?: 'hover' | 'click' | 'contextmenu';
+    /** 下拉触发元素呈现为按钮组 */
+    splitButton?: boolean;
+    /** 菜单按钮类型，同 Button 组件一样，仅在 split-button 为 true 的情况下有效。 */
+    type?: ButtonProps['type'];
+    /** 菜菜单尺寸，在 split-button 为 true 的情况下也对触发按钮生效。 */
+    size?: ButtonProps['size'];
+    /** 按钮组件的 props */
+    buttonProps?: ButtonProps;
+    /** 菜单最大高度 */
+    maxHeight?: number | string;
+    /** 默认提供的主题 */
+    effect?: 'light' | 'dark' | string;
     /** 是否在点击菜单项后隐藏菜单 */
     hideOnClick?: boolean;
     /** 展开下拉菜单的延时，仅在 trigger 为 hover 时有效 */
@@ -25,20 +40,24 @@ export interface DropdownProps
     defaultVisible?: boolean;
     /** 是否禁用 */
     disabled?: boolean;
+    /** 用来标识虚拟触发是否被启用 */
+    virtualTriggering?: boolean;
+    /** 标识虚拟触发时的触发元素 */
+    virtualRef?: VirtualElement;
     /** [popper.js](https://popper.js.org/docs/v2/) 参数 */
     // popperOptions?: PopperOptions;
     onMouseEnter?: (e?: React.MouseEvent<any>) => void;
     onMouseLeave?: (e?: React.MouseEvent<any>) => void;
+    /** splitButton 为 true 时，点击左侧按钮的回调 */
+    onClick?: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
     /** 点击菜单项触发的事件回调 */
-    onClick?: (command: string | number | object) => void;
+    onCommand?: (command: string | number | object) => void;
     /** 下拉框出现/隐藏时触发: 出现则为 true，隐藏则为 false */
-    onVisiblechange?: (visible: boolean) => void;
+    onVisibleChange?: (visible: boolean) => void;
 }
 
 export interface DropdownMenuProps extends BaseProps, NativeProps {
     classPrefix?: string;
-    /** 菜单最大高度 */
-    maxHeight?: number;
 }
 
 /** Dropdown-Item 属性 */
